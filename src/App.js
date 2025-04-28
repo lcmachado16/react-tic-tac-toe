18513877b4e28b6441cacbd8ac91795675f6967b
lcmachado16 <lcmachado@inf.ufrgs.br>
2025-04-28 15:08:18 -0300

    onSquare[0] click

--- src/App.js
+++ src/App.js
@@ -2,39 +2,49 @@ import { useState } from "react";
 
 
 export default function Board() {
+  const [squares,setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(){
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <> 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
 }
 
-function Square() {
-  // value stores the value and setValue is a function that can be used to change the value
-  const [value,setValue] = useState(null);
+function Square({value, onSquareClick}) {
+  // return <button className="square">{value}</button>
 
-  function handleClick(){
-    setValue('X');
-  }
+  // value stores the value and setValue is a function that can be used to change the value
+  // const [value,setValue] = useState(null);
+
+  // function handleClick(){
+  //   setValue('X');
+  // }
 
   return (
   <button 
     className="square"
-    onClick={handleClick}
+    onClick={onSquareClick}
   >
     {value}
     </button>
