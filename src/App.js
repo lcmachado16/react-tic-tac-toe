e5fe62aece7b14c1d25265107afb3d345bfab8e1
lcmachado16 <lcmachado@inf.ufrgs.br>
2025-04-28 17:00:07 -0300

    remove key error

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description = move > 0 ? 'Go to move #' + move : 'Go to game start';
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     )
