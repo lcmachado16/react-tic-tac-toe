f497d45f3d5831822b8efcdd8bcea2d117f31c63
lcmachado16 <lcmachado@inf.ufrgs.br>
2025-04-28 14:42:06 -0300

    setValue() -> draw a 'X' on click

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
+import { useState } from "react";
+
+
 export default function Board() {
   return (
-  <> 
-  <div className="board-row">
-    <Square value="1" />
-    <Square value="2" />
-    <Square value="3" />
-  </div>
-  <div className="board-row">
-    <Square value="4" />
-    <Square value="5" />
-    <Square value="6" />
-  </div>
-  <div className="board-row">
-    <Square value="7" />
-    <Square value="8" />
-    <Square value="9" />
-  </div>
-  </>
-)
+    <> 
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
 
-function Square({ value }) {
+function Square() {
+  // value stores the value and setValue is a function that can be used to change the value
+  const [value,setValue] = useState(null);
+
   function handleClick(){
-    console.log("clicked!");
+    setValue('X');
   }
 
   return (
