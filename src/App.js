47c555b950c9f1917935bb844b4544a75257a368
lcmachado16 <lcmachado@inf.ufrgs.br>
2025-04-28 15:33:25 -0300

    Taking Turns

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares,setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
+    if (squares[i] != null) { return;} // Keeps the state
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    nextSquares[i] = xIsNext ? "X" : "O"; 
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
